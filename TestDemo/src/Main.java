90a430c461357ef6c2f5e45c18619518177740c5
123 <897131608@qq.com>
2020-01-22 22:24:01 +0800

    提交字符串习题代码

--- TestDemo/src/Main.java
+++ TestDemo/src/Main.java
@@ -241,6 +241,49 @@ public class Main {
         }
     }
 
+    /**
+     * 58. 最后一个单词的长度
+     */
+    public static int lengthOfLastWord(String s) {
+        if(s == null) {
+            return 0;
+        }
+
+        int count = 0;
+        for(int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) != ' ') {
+                count++;
+            } else {
+                count = 0;
+            }
+        }
+        return count;
+    }
+
+    public static int lengthOfLastWord1(String s) {
+        if(s == null && s.length() == 0) {
+            return 0;
+        }
+        s.trim();
+        int last = s.length()-1;
+        int count = 0;
+        for (int i = last; i >= 0; i--) {
+            if (s.charAt(i) == ' ') {
+                if (count == 0){
+                    continue;
+                }
+                break;
+            }
+            count++;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        String s = "a";
+        int a = lengthOfLastWord1(s);
+        System.out.println(a);
+    }
 
 
 
