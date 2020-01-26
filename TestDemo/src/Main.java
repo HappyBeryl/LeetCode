6dd2129546e6f4255ce37ba3b5a92d89859944e4
123 <897131608@qq.com>
2020-01-26 23:07:22 +0800

    提交测试习题代码

--- TestDemo/src/Main.java
+++ TestDemo/src/Main.java
@@ -1,3 +1,4 @@
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class Main {
@@ -8,7 +9,7 @@ public class Main {
      */
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> triangle = new ArrayList<>();
-        if(numRows == 0) {
+        if (numRows == 0) {
             return triangle;
         }
         triangle.add(new ArrayList<>()); //0行
@@ -18,10 +19,10 @@ public class Main {
             List<Integer> curRow = new ArrayList<>();
             curRow.add(1);
             //上一行
-            List<Integer> prevRow = triangle.get(i-1);
+            List<Integer> prevRow = triangle.get(i - 1);
             //第i行第j列
             for (int j = 1; j < i; j++) {
-                int tmp = prevRow.get(j-1)+prevRow.get(j);
+                int tmp = prevRow.get(j - 1) + prevRow.get(j);
                 curRow.add(tmp);
             }
             curRow.add(1);
@@ -37,18 +38,18 @@ public class Main {
      */
     public boolean isValid(String s) {
         Stack<Character> stack = new Stack<>();
-        for(int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
-            if(ch == '(' || ch == '{' || ch == '[') {
+            if (ch == '(' || ch == '{' || ch == '[') {
                 stack.push(ch);
             } else {
-                if(stack.empty()) {
+                if (stack.empty()) {
                     System.out.println("右括号多");
                     return false;
                 }
                 char top = stack.peek();
-                if(top == '(' && ch == ')' ||top == '{' && ch == '}'
-                        ||top == '[' && ch == ']'){
+                if (top == '(' && ch == ')' || top == '{' && ch == '}'
+                        || top == '[' && ch == ']') {
                     stack.pop();
                 } else {
                     System.out.println("右括号匹配错误");
@@ -57,7 +58,7 @@ public class Main {
             }
         }
 
-        if(!stack.empty()) {
+        if (!stack.empty()) {
             System.out.println("左括号多");
             return false;
         }
@@ -75,34 +76,40 @@ public class Main {
         public Queue<Integer> queue2 = new LinkedList<>();
 
 
-        /** Initialize your data structure here. */
+        /**
+         * Initialize your data structure here.
+         */
         public MyStack() {
 
         }
 
-        /** Push element x onto stack. */
+        /**
+         * Push element x onto stack.
+         */
         public void push(int x) {
-            if(!queue1.isEmpty()) {
+            if (!queue1.isEmpty()) {
                 queue1.offer(x);
-            }else if(!queue2.isEmpty()){
+            } else if (!queue2.isEmpty()) {
                 queue2.offer(x);
-            }else {
+            } else {
                 queue1.offer(x);
             }
         }
 
-        /** Removes the element on top of the stack and returns that element. */
+        /**
+         * Removes the element on top of the stack and returns that element.
+         */
         public int pop() {
-            if(!queue1.isEmpty()) {
-                int size1 = queue1.size()-1;
+            if (!queue1.isEmpty()) {
+                int size1 = queue1.size() - 1;
                 //因为：queue1.poll()--》queue1.size()发生改变
                 for (int i = 0; i < size1; i++) {
                     queue2.offer(queue1.poll());
                 }
                 return queue1.poll();
             }
-            if(!queue2.isEmpty()){
-                int size2 = queue2.size()-1;
+            if (!queue2.isEmpty()) {
+                int size2 = queue2.size() - 1;
                 for (int i = 0; i < size2; i++) {
                     queue1.offer(queue2.poll());
                 }
@@ -111,9 +118,11 @@ public class Main {
             return -1;
         }
 
-        /** Get the top element. */
+        /**
+         * Get the top element.
+         */
         public int top() {
-            if(!queue1.isEmpty()) {
+            if (!queue1.isEmpty()) {
                 int tmp = 0;
                 int size1 = queue1.size();
                 for (int i = 0; i < size1; i++) {
@@ -122,7 +131,7 @@ public class Main {
                 }
                 return tmp;
             }
-            if(!queue2.isEmpty()){
+            if (!queue2.isEmpty()) {
                 int tmp2 = 0;
                 int size2 = queue2.size();
                 for (int i = 0; i < size2; i++) {
@@ -134,9 +143,11 @@ public class Main {
             return -1;
         }
 
-        /** Returns whether the stack is empty. */
+        /**
+         * Returns whether the stack is empty.
+         */
         public boolean empty() {
-            if(queue1.isEmpty() && queue2.isEmpty()){
+            if (queue1.isEmpty() && queue2.isEmpty()) {
                 return true;
             }
             return false;
@@ -154,44 +165,54 @@ public class Main {
 
         private Stack<Integer> stack2;
 
-        /** Initialize your data structure here. */
+        /**
+         * Initialize your data structure here.
+         */
         public MyQueue() {
             stack1 = new Stack<>();
             stack2 = new Stack<>();
         }
 
-        /** Push element x to the back of queue. */
+        /**
+         * Push element x to the back of queue.
+         */
         public void push(int x) {
             stack1.push(x);
         }
 
-        /** Removes the element from in front of queue and returns that element. */
+        /**
+         * Removes the element from in front of queue and returns that element.
+         */
         public int pop() {
-            if(stack2.empty()) {
+            if (stack2.empty()) {
                 while (!stack1.empty()) {
                     stack2.push(stack1.pop());
                 }
             }
-            if(!stack2.empty()) {
+            if (!stack2.empty()) {
                 return stack2.pop();
             }
             return -1;
         }
 
-        /** Get the front element. */
+        /**
+         * Get the front element.
+         */
         public int peek() {
-            if(stack2.empty()) {
+            if (stack2.empty()) {
                 while (!stack1.empty()) {
                     stack2.push(stack1.pop());
                 }
             }
-            if(!stack2.empty()) {
+            if (!stack2.empty()) {
                 return stack2.peek();
             }
             return -1;
         }
 
-        /** Returns whether the queue is empty. */
+        /**
+         * Returns whether the queue is empty.
+         */
         public boolean empty() {
             return stack2.empty() && stack1.empty();
         }
@@ -205,7 +226,9 @@ public class Main {
      */
 
     class MinStack {
-        /** initialize your data structure here. */
+        /**
+         * initialize your data structure here.
+         */
         public Stack<Integer> stack = new Stack<>();
         public Stack<Integer> MinStack = new Stack<>();
 
@@ -215,11 +238,11 @@ public class Main {
 
         void push(int x) {
             stack.push(x);
-            if(MinStack.empty()) {
+            if (MinStack.empty()) {
                 MinStack.push(x);
             } else {
                 int tmp = MinStack.peek();
-                if(x <= tmp) {
+                if (x <= tmp) {
                     MinStack.push(x);
                 }
             }
@@ -227,7 +250,7 @@ public class Main {
 
         void pop() {
             int tmp = stack.pop();
-            if(tmp == MinStack.peek()) {
+            if (tmp == MinStack.peek()) {
                 MinStack.pop();
             }
         }
@@ -245,13 +268,13 @@ public class Main {
      * 58. 最后一个单词的长度
      */
     public static int lengthOfLastWord(String s) {
-        if(s == null) {
+        if (s == null) {
             return 0;
         }
 
         int count = 0;
-        for(int i = 0; i < s.length(); i++) {
-            if(s.charAt(i) != ' ') {
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != ' ') {
                 count++;
             } else {
                 count = 0;
@@ -261,15 +284,15 @@ public class Main {
     }
 
     public static int lengthOfLastWord1(String s) {
-        if(s == null && s.length() == 0) {
+        if (s == null && s.length() == 0) {
             return 0;
         }
         s.trim();
-        int last = s.length()-1;
+        int last = s.length() - 1;
         int count = 0;
         for (int i = last; i >= 0; i--) {
             if (s.charAt(i) == ' ') {
-                if (count == 0){
+                if (count == 0) {
                     continue;
                 }
                 break;
@@ -287,22 +310,22 @@ public class Main {
 
     /**
      * 88.合并两个有序数组
-     从后往前比较，把较大的放在nums1的最后
-     当nums2没有比较完，直接加在最后（有序数组）
+     * 从后往前比较，把较大的放在nums1的最后
+     * 当nums2没有比较完，直接加在最后（有序数组）
      */
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = m-1;
-        int j = n-1;
-        int last = nums1.length-1;
-        while(i >= 0 && j >=0) {
-            if(nums2[j] > nums1[i]) {
+        int i = m - 1;
+        int j = n - 1;
+        int last = nums1.length - 1;
+        while (i >= 0 && j >= 0) {
+            if (nums2[j] > nums1[i]) {
                 nums1[last--] = nums2[j--];
             } else {
                 nums1[last--] = nums1[i--];
             }
         }
 
-        while(i < 0 && j >= 0) {
+        while (i < 0 && j >= 0) {
             nums1[last--] = nums2[j--];
         }
     }
@@ -314,9 +337,9 @@ public class Main {
     public boolean isPalindrome(int x) {
         String s = x + "";
         int leftIndex = 0;
-        int rightIndex = s.length()-1;
-        while(leftIndex <= rightIndex) {
-            if(s.charAt(leftIndex++) != s.charAt(rightIndex--)) {
+        int rightIndex = s.length() - 1;
+        while (leftIndex <= rightIndex) {
+            if (s.charAt(leftIndex++) != s.charAt(rightIndex--)) {
                 return false;
             }
         }
@@ -326,17 +349,17 @@ public class Main {
     //法2：以上做法转化为字符串会消耗额外的空间，利用回文数的折叠相等，翻转后半部分
     public boolean isPalindrome1(int x) {
         //处理特殊情况
-        if(x < 0 || x%10 == 0 && x != 0) {
+        if (x < 0 || x % 10 == 0 && x != 0) {
             return false;
         }
         //翻转后半部分
         int reverseNum = 0;
-        while(x > reverseNum) {
-            reverseNum = reverseNum*10 + x%10;
-            x = x/10;
+        while (x > reverseNum) {
+            reverseNum = reverseNum * 10 + x % 10;
+            x = x / 10;
         }
         //处理偶数位数字和奇数位数字
-        return reverseNum == x || reverseNum/10 == x;
+        return reverseNum == x || reverseNum / 10 == x;
     }
 
     /**
@@ -344,8 +367,8 @@ public class Main {
      */
     public boolean containsDuplicate(int[] nums) {
         Arrays.sort(nums);
-        for(int i = 0; i < nums.length-1; i++) {
-            if(nums[i] == nums[i+1]) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
                 return true;
             }
         }
@@ -354,23 +377,23 @@ public class Main {
 
 
     public static boolean isLongPressedName1(String name, String typed) {
-        if(typed == null || typed.length() == 0) {
+        if (typed == null || typed.length() == 0) {
             return false;
         }
         StringBuffer sb = new StringBuffer();
         int i = 0;
-        while(i < typed.length()-1) {
-            if(typed.charAt(i) != typed.charAt(i+1)) {
+        while (i < typed.length() - 1) {
+            if (typed.charAt(i) != typed.charAt(i + 1)) {
                 sb.append(typed.charAt(i));
-                sb.append(typed.charAt(i+1));
+                sb.append(typed.charAt(i + 1));
                 i++;
             } else {
-                while((typed.charAt(i)+"") == (typed.charAt(i+1)+"")) {
+                while ((typed.charAt(i) + "") == (typed.charAt(i + 1) + "")) {
                     i++;
                 }
             }
         }
-        if(typed.charAt(i) != typed.charAt(i-1)) {
+        if (typed.charAt(i) != typed.charAt(i - 1)) {
             sb.append(typed.charAt(i));
         }
         return sb.toString().equals(name);
@@ -379,28 +402,133 @@ public class Main {
     public static boolean isLongPressedName(String name, String typed) {
         int len_n = name.length();
         int len_t = typed.length();
-        if(len_n>len_t){
+        if (len_n > len_t) {
             return false;
         }
-        int i=0;
-        int j=0;
-        while(i<len_n&&j<len_t){
-            if(name.charAt(i)==typed.charAt(j)){
+        int i = 0;
+        int j = 0;
+        while (i < len_n && j < len_t) {
+            if (name.charAt(i) == typed.charAt(j)) {
                 i++;
                 j++;
-            }else if(j>0&&typed.charAt(j)==typed.charAt(j-1)){
+            } else if (j > 0 && typed.charAt(j) == typed.charAt(j - 1)) {
                 j++;
-            }else{
+            } else {
                 return false;
             }
         }
-        return i ==len_n;
+        return i == len_n;
     }
 
-    public static void main(String[] args) {
-        boolean bol = isLongPressedName("da","daad");
-        System.out.println(bol);
+    /**
+     * 977.有序数组的平方
+     */
+    public int[] sortedSquares(int[] A) {
+        int tmp[] = new int[A.length];
+        for (int i = 0; i < A.length; i++) {
+            tmp[i] = A[i] * A[i];
+        }
+        Arrays.sort(tmp);
+        return tmp;
     }
+
+    //法2：双指针 i记录负数 j记录正数
+    public static int[] sortedSquares1(int[] A) {
+        int j = 0;
+        int ret[] = new int[A.length];
+        int n = 0;
+        while (A[j] < 0) {
+            j++;
+        }
+        int i = j - 1;
+        while (i >= 0 && j < A.length) {
+            if (A[i] * A[i] < A[j] * A[j]) {
+                ret[n] = A[i] * A[i];
+                i--;
+            } else {
+                ret[n] = A[j] * A[j];
+                j++;
+            }
+            n++;
+        }
+        if (i < 0 && j < A.length && n < A.length) {
+            while (j < A.length) {
+                ret[n] = A[j] * A[j];
+                j++;
+                n++;
+            }
+        }
+        if (i >= 0 && j >= A.length && n < A.length) {
+            while (i >= 0) {
+                ret[n] = A[i] * A[i];
+                i--;
+                n++;
+            }
+        }
+        return ret;
+    }
+
+    public static void main977(String[] args) {
+        int[] array = new int[]{-4, -1, 0, 3, 10};
+        int ret[] = sortedSquares1(array);
+        System.out.println(Arrays.toString(ret));
+    }
+
+    /**
+     * 917.仅仅翻转字母
+     */
+
+    //双指针
+    public static String reverseOnlyLetters(String S) {
+        int j = S.length() - 1;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < S.length(); i++) {
+            if (Character.isLetter(S.charAt(i))) {
+                while (!Character.isLetter(S.charAt(j))) {
+                    j--;
+                }
+                sb.append(S.charAt(j--));
+            } else {
+                sb.append(S.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+
+    //用栈
+    public String reverseOnlyLetters1(String S) {
+        Stack stack = new Stack();
+        for(int i = 0; i < S.length(); i++) {
+            if(Character.isLetter(S.charAt(i))) {
+                stack.push(S.charAt(i));
+            }
+        }
+
+        StringBuffer sb = new StringBuffer();
+        for(int i = 0; i < S.length(); i++) {
+            if(Character.isLetter(S.charAt(i))) {
+                sb.append(stack.pop());
+            } else {
+                sb.append(S.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+
+
+    public static void main917(String[] args) {
+        String s = reverseOnlyLetters("a-bj987");
+        System.out.println(s);
+    }
+
+
+
+
+
+
+
+
+
 
 }
 
