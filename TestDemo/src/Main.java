b2371783e216a0ad2406d9b8ec966bffc85f3d85
123 <897131608@qq.com>
2020-02-14 22:14:36 +0800

    提交测试习题代码

--- TestDemo/src/Main.java
+++ TestDemo/src/Main.java
@@ -1167,6 +1167,27 @@ public class Main {
         }
     }
 
+    /*
+    反转字符串
+     */
+    public String reverseOnlyLetter(String S) {
+        Stack stack = new Stack();
+        for(int i = 0; i < S.length(); i++) {
+            if(Character.isLetter(S.charAt(i))) {
+                stack.push(S.charAt(i));
+            }
+        }
+        StringBuffer sb = new StringBuffer();
+        for(int i = 0; i < S.length(); i++) {
+            if(Character.isLetter(S.charAt(i))) {
+                sb.append(stack.pop());
+            } else {
+                sb.append(S.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+
 
 
 
