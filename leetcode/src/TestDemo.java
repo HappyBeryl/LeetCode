353a09bd583a91d016eacea65a3acca60d15c07e
123 <897131608@qq.com>
2020-03-02 22:33:19 +0800

    寻找第k大

--- leetcode/src/TestDemo.java
+++ leetcode/src/TestDemo.java
@@ -22,4 +22,45 @@ public class TestDemo {
         System.out.println(count);
     }
 
+    /*
+    寻找第k大
+     */
+    private static int partion(int[] array, int start, int end) {
+        int tmp = array[start];
+        while (start < end) {
+            while ((start < end) && array[end] <= tmp) {
+                end--;
+            }
+            if (start >= end) {
+                break;
+            } else {
+                array[start] = array[end];
+            }
+            while ((start < end) && array[start] >= tmp) {
+                start++;
+            }
+            if (start >= end) {
+                //array[start] = tmp;
+                break;
+            } else {
+                array[end] = array[start];
+            }
+        }
+        array[start] = tmp;
+        return start;
+    }
+
+    public int findKthChild(int[] array, int low, int high, int k) {
+        int pivot = partion(array, low, high);
+
+        if(k == pivot-low+1) return array[pivot];
+        else if(k > pivot-low+1) return findKthChild(array, pivot+1, high, k-(pivot-low+1));
+        else return findKthChild(array, low, pivot-1, k);
+    }
+
+    public int findKth(int[] a, int n, int K) {
+        // write code here
+        return findKthChild(a, 0, n-1, K);
+    }
+
 }
