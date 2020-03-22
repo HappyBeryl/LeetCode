efce1ba951f10cf9b4d2ceee5557d5955a64bd94
123 <897131608@qq.com>
2020-03-22 23:17:30 +0800

    二维数组中的查找

--- leetcode/src/JZoffer.java
+++ leetcode/src/JZoffer.java
@@ -15,5 +15,28 @@ public class JZoffer {
         return -1;
     }
 
+    /*
+    二维数组中的查找
+     */
+    public boolean findNumberIn2DArray(int[][] matrix, int target) {
+        if(matrix == null || matrix.length == 0) {
+            return false;
+        }
+        int m = matrix.length; //二维数组的行数
+        int n = matrix[0].length; // 二维数组的列数
+        int row = 0;
+        int col = n - 1;
+        while(row < m && col >= 0) {
+            if(matrix[row][col] == target) {
+                return true;
+            } else if(matrix[row][col] > target) {
+                col--;
+            } else {
+                row++;
+            }
+        }
+        return false;
+    }
+
 
 }
