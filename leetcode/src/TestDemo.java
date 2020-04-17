c940103848cc115489985adb0e96c15ffa686153
123 <897131608@qq.com>
2020-04-17 11:24:37 +0800

    坐标移动和连续最大和

--- leetcode/src/TestDemo.java
+++ leetcode/src/TestDemo.java
@@ -1708,6 +1708,54 @@ public class TestDemo {
         }
     }
 
+    public static void main68(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine()) {
+            String s = sc.nextLine();
+            String[] s1 = s.split(";");
+            int x = 0, y = 0;
+            for (int i = 0; i < s1.length; i++) {
+                if (s1[i].length() >= 2 && isNumber(s1[i].substring(1))) {
+                    if (s1[i].charAt(0) == 'A') {
+                        x -= Integer.parseInt(s1[i].substring(1));
+                    } else if (s1[i].charAt(0) == 'D') {
+                        x += Integer.parseInt(s1[i].substring(1));
+                    } else if (s1[i].charAt(0) == 'S') {
+                        y -= Integer.parseInt(s1[i].substring(1));
+                    } else if (s1[i].charAt(0) == 'W') {
+                        y += Integer.parseInt(s1[i].substring(1));
+                    }
+                }
+            }
+            System.out.println(x + "," + y);
+        }
+    }
+    private static boolean isNumber(String s){
+        for (int i = 0; i <s.length() ; i++) {
+            if(!Character.isDigit(s.charAt(i))){
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main69(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        int n = sc.nextInt();
+        while(n-- > 0){
+            sum+=sc.nextInt();
+            if(sum>max){
+                max = sum;
+            }
+            if(sum<0){
+                sum = 0;
+            }
+        }
+        System.out.println(max);
+    }
+
 
 
 
