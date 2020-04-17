5dd03b168822155403d3dee3ce573799b88fcb24
123 <897131608@qq.com>
2020-04-17 22:36:48 +0800

    提交测试代码

--- leetcode/src/TestDemo.java
+++ leetcode/src/TestDemo.java
@@ -1756,6 +1756,26 @@ public class TestDemo {
         System.out.println(max);
     }
 
+    public int[] arrayPrint1(int[][] arr, int n) {
+        // write code here
+        int[] res = new int[n * n];
+        int index = 0;
+        int startX = 0;
+        int startY = n - 1;
+        while (startX < n) {
+            int x = startX;
+            int y = startY;
+            while (x < n && y < n)
+                res[index++] = arr[x++][y++];
+            if (startY > 0) startY--;//确定新的开始纵坐标
+
+            else
+                startX++;
+            //确定新的开始横坐标
+        }
+        return res;
+    }
+
 
 
 
