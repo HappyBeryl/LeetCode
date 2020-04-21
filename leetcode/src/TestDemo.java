e9bf8045ee77cfabfcd41d337a3d5bbf5c681520
123 <897131608@qq.com>
2020-04-21 22:24:21 +0800

    提交测试代码

--- leetcode/src/TestDemo.java
+++ leetcode/src/TestDemo.java
@@ -1858,7 +1858,7 @@ public class TestDemo {
         System.out.println(sb);
     }
 
-    public static void main(String[] args) {
+    public static void main72(String[] args) {
       //  Scanner sc = new Scanner(System.in);
       //  int n = sc.nextInt();
       //  int m = sc.nextInt();
@@ -1881,6 +1881,32 @@ public class TestDemo {
             }
         }
 
+    }
+
+    public static void main73(String[] args){
+        String symbol="ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        String number="222333444555666777788899991234567890";
+        Scanner scanner=new Scanner(System.in);
+        while(scanner.hasNext()){
+            int n=scanner.nextInt();
+            ArrayList<String> arrayList=new ArrayList<String>();
+            for(int i=0;i<n;i++){
+                String str=scanner.next();
+                str=str.replace("-","");
+                String result="";
+                for(int j=0;j<7;j++){
+                    result+=number.charAt(symbol.indexOf(str.charAt(j)+""));
+                }
+                result=result.substring(0,3)+"-"+result.substring(3,7);
+                if(!arrayList.contains(result))
+                    arrayList.add(result);
+            }
+            Collections.sort(arrayList);
+            for(int j=0;j<arrayList.size();j++){
+                System.out.println(arrayList.get(j));
+            }
+            System.out.println();
+        }
     }
 
 
