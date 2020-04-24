e423cc65240ffe14be7665396eafd483a67286da
123 <897131608@qq.com>
2020-04-24 23:24:53 +0800

    提交测试习题代码

--- leetcode/src/TestDemo.java
+++ leetcode/src/TestDemo.java
@@ -2056,7 +2056,7 @@ public class TestDemo {
         }
     }
 
-    public static void main78(String[] args) {
+    public static void main79(String[] args) {
         Scanner cin = new Scanner(System.in);
         while (cin.hasNextInt()) {
             int n = cin.nextInt();          // n 个学生
@@ -2084,6 +2084,53 @@ public class TestDemo {
                 res = Math.max(res ,fmax[K][i]);
             }
             System.out.println(res);
+        }
+    }
+
+    static class People {
+        int height;
+        int weight;
+
+        public People(int weight, int height) {
+            this.height = height;
+            this.weight = weight;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        while (scan.hasNext()) {
+            int n = scan.nextInt();
+            People[] array = new People[n];
+            for (int i = 0; i < n; ++i) {
+                int index = scan.nextInt();
+                array[index - 1] = new People(scan.nextInt(), scan.nextInt());
+            }
+
+            Arrays.sort(array, new Comparator<People>() {
+                public int compare(People p1, People p2) {
+                    int result = Integer.compare(p1.height, p2.height);
+                    if (result != 0)
+                        return result;
+                    else
+                        return Integer.compare(p1.weight, p2.weight);
+                }
+            });
+
+            int[] dp = new int[n];
+            int max = Integer.MIN_VALUE;
+            for (int i = 0; i < dp.length; ++i) {
+                dp[i] = 1;
+                for (int j = i - 1; j >= 0; --j) {
+                    if (array[i].weight > array[j].weight
+                            || (array[i].weight == array[j].weight && array[i].height == array[j].height)) {
+                        dp[i] = Math.max(dp[i], dp[j] + 1);
+                    }
+                }
+                max = Math.max(dp[i], max);
+            }
+            System.out.println(max);
         }
     }
 
