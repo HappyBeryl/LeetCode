d7f361d11333bbf7caf616ea8872516d0b35f86b
123 <897131608@qq.com>
2020-04-28 22:14:20 +0800

    两个栈实现队列

--- leetcode/src/JZoffer.java
+++ leetcode/src/JZoffer.java
@@ -166,5 +166,54 @@ public class JZoffer {
         return buildTreeChild1(inorder, postorder, 0, inorder.length-1);
     }
 
+     class MyQueue {
+
+        private Stack<Integer> stack1;
+
+        private Stack<Integer> stack2;
+
+        /** Initialize your data structure here. */
+        public MyQueue() {
+            stack1 = new Stack<>();
+            stack2 = new Stack<>();
+        }
+
+        /** Push element x to the back of queue. */
+        public void push(int x) {
+            stack1.push(x);
+        }
+
+        /** Removes the element from in front of queue and returns that element. */
+        public int pop() {
+            if(stack2.empty()) {
+                while (!stack1.empty()) {
+                    stack2.push(stack1.pop());
+                }
+            }
+            if(!stack2.empty()) {
+                return stack2.pop();
+            }
+            return -1;
+        }
+
+        /** Get the front element. */
+        public int peek() {
+            if(stack2.empty()) {
+                while (!stack1.empty()) {
+                    stack2.push(stack1.pop());
+                }
+            }
+            if(!stack2.empty()) {
+                return stack2.peek();
+            }
+            return -1;
+        }
+
+        /** Returns whether the queue is empty. */
+        public boolean empty() {
+            return stack2.empty() && stack1.empty();
+        }
+    }
+
 
 }
