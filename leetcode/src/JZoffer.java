1ab68d1cbd47944b3cc115d330e4fab065daf862
123 <897131608@qq.com>
2020-05-01 23:03:22 +0800

    斐波那契数列

--- leetcode/src/JZoffer.java
+++ leetcode/src/JZoffer.java
@@ -216,4 +216,19 @@ public class JZoffer {
     }
 
 
+    public int fib(int n) {
+        int f1 = 0;
+        int f2 = 1;
+        int f3 = 0;
+        for(int i = 0; i < n; i++) {
+            f3 = (f1 + f2) % 1000000007;
+            f1 = f2;
+            f2 = f3;
+        }
+        return f1;
+    }
+
+    
+
+
 }
