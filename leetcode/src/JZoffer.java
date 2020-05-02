27837f79370bb8e0d38e407a50abedde794e3bc5
123 <897131608@qq.com>
2020-05-02 23:40:08 +0800

    矩阵中的路径

--- leetcode/src/JZoffer.java
+++ leetcode/src/JZoffer.java
@@ -262,5 +262,35 @@ public class JZoffer {
         return numbers[i];
     }
 
+    public boolean exist(char[][] board, String word) {
+        if (board == null || board[0] == null || board.length == 0 || board[0].length == 0 ||        word == null || word.equals("")) {
+            return false;
+        }
+        char[] words = word.toCharArray();
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[0].length; j++) {
+                if(existChild(board, words, i, j, 0)) return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean existChild(char[][] board, char[] word, int i, int j, int k) {
+        if(i >= board.length || i < 0 || j >= board[0].length || j < 0 || board[i][j] != word[k]) {
+            return false;
+        }
+        if(k == word.length - 1) return true;
+        char tmp = board[i][j];
+        // 修改为字符 '/' ，代表此元素已访问过，防止之后搜索时重复访问。
+        board[i][j] = '/';
+        boolean res = existChild(board, word, i + 1, j, k + 1) ||
+                existChild(board, word, i - 1, j, k + 1) ||
+                existChild(board, word, i, j + 1, k + 1) ||
+                existChild(board, word, i , j - 1, k + 1);
+        board[i][j] = tmp;
+        return res;
+    }
+
+
 
 }
