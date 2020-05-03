773de722acf7b5f1065c3e8720c67c4f5c435a1f
123 <897131608@qq.com>
2020-05-03 22:53:48 +0800

    二进制中1的个数

--- leetcode/src/JZoffer.java
+++ leetcode/src/JZoffer.java
@@ -291,6 +291,26 @@ public class JZoffer {
         return res;
     }
 
+    public static int PrintOneNum(int n) {
+        int count = 0;
+        while (n != 0) {
+            if (n % 2 == 1) {
+                count++;
+            }
+            n /= 2;
+        }
+        return count;
+    }
+
+    public int hammingWeight(int n) {
+        int count = 0;
+        while (n != 0) {
+            count++;
+            n = n & (n - 1);
+        }
+        return count;
+
+    }
 
 
 }
