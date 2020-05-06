1b3a98a8fece2cca335e25b8cb6e44130f99f1dc
123 <897131608@qq.com>
2020-05-06 23:11:53 +0800

    链表中倒数第k个节点

--- leetcode/src/JZoffer.java
+++ leetcode/src/JZoffer.java
@@ -370,5 +370,25 @@ public class JZoffer {
         return arr;
     }
 
+    public ListNode getKthFromEnd(ListNode head, int k) {
+        if (k <= 0 || head == null) {
+            return null;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        for (int i = 0; i < k-1; i++) {
+            if (fast.next == null) {
+                return null;
+            }
+            fast = fast.next;
+        }
+        while(fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+
+        }
+        return slow;
+    }
+
 
 }
