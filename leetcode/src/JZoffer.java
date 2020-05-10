8a0f9815107f96602847ae50a3f7472d032e78a7
123 <897131608@qq.com>
2020-05-10 23:02:45 +0800

    剪绳子

--- leetcode/src/JZoffer.java
+++ leetcode/src/JZoffer.java
@@ -437,9 +437,9 @@ public class JZoffer {
             b = -b;
         }
         while(b > 0) {
-            if((b & 1) == 1) res *= x;
-            x *= x;
-            b >>= 1;
+            if((b & 1) == 1) res *= x; //b%2 == 1
+            x *= x; //x = x^2
+            b >>= 1; //b /= 2
         }
         return res;
     }
@@ -452,6 +452,21 @@ public class JZoffer {
         double mod = myPow(x, n % 2);
         return half * half * mod;
     }
+
+    public int cuttingRope(int n) {
+        if (n < 2) {
+            return 0;
+        }
+        int[] dp = new int[n + 1];
+        dp[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                dp[i] = Math.max(Math.max(j * dp[i - j], j * (i - j)), dp[i]);
+            }
+        }
+        return dp[n];
+    }
+    
 
 
 
