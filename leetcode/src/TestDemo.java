bf1363ca48738dc7f7ddeb8e9996ade4a7001ba1
123 <897131608@qq.com>
2020-05-16 22:54:58 +0800

    提交测试习题代码

--- leetcode/src/TestDemo.java
+++ leetcode/src/TestDemo.java
@@ -2516,94 +2516,28 @@ public class TestDemo {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-
-        Mode mA = new Mode();
-        Mode mB = new Mode();
-
-        for (int i = 0; i < N; i++) {
-            String a = sc.next();
-            String b = sc.next();
-            judges(a, b, mA, mB);
-        }
-
-        System.out.println(mA.win + " " + mA.tie + " " + mA.lose);
-        System.out.println(mB.win + " " + mB.tie + " " + mB.lose);
-        System.out.println(getMostGen(mA.map) + " " + getMostGen(mB.map));
-    }
-
-    public static void judges(String a, String b, Model mA,
-                             Model mB) {
-        if (a.equals("C")) {
-            if (b.equals("C")) {
-                mA.tie++;
-                mB.tie++;
-            } else if (b.equals("J")) {
-                mA.win++;
-                mB.lose++;
-                mA.map.put("C", mA.map.get("C") + 1);
-            } else {
-                mA.lose++;
-                mB.win++;
-                mB.map.put("B", mA.map.get("B") + 1);
-            }
-        } else if (a.equals("J")) {
-            if (b.equals("C")) {
-                mA.lose++;
-                mB.win++;
-                mB.map.put("C", mA.map.get("C") + 1);
-            } else if (b.equals("J")) {
-                mA.tie++;
-                mB.tie++;
-            } else {
-                mA.win++;
-                mB.lose++;
-                mA.map.put("J", mA.map.get("J") + 1);
-            }
-        } else {
-            if (b.equals("C")) {
-                mA.win++;
-                mB.lose++;
-                mA.map.put("B", mA.map.get("B") + 1);
-            } else if (b.equals("J")) {
-                mA.lose++;
-                mB.win++;
-                mB.map.put("J", mA.map.get("J") + 1);
-            } else {
-                mA.tie++;
-                mB.tie++;
-            }
+        while(sc.hasNext()) {
+            int n = sc.nextInt();
+            float result = (func1(n) / func2(n)) * 100;
+            System.out.println(String.format("%.2f", result) + "%");
         }
     }
 
-    public static String getMostGens(Map<String, Integer> map) {
-        if (map.get("C") >= map.get("J")) {
-            if (map.get("C") > map.get("B")) {
-                return "C";
-            } else {
-                return "B";
-            }
-        } else {
-            if (map.get("J") > map.get("B")) {
-                return "J";
-            } else {
-                return "B";
-            }
+    public static float func1(int n) {
+        if(n == 1) {
+            return 0;
         }
+        if(n == 2) {
+            return 1;
+        }
+        return (n-1) * (func1(n-2) + func1(n-1));
     }
 
-    static class Mode {
-        int win;
-        int tie;
-        int lose;
-        Map<String, Integer> map;
-
-        Mode() {
-            map = new HashMap<>();
-            map.put("B", 0);
-            map.put("C", 0);
-            map.put("J", 0);
+    public static float func2(int n) { 
+        if(n == 0) {
+            return 1;
         }
+        return n * func2(n-1);
     }
 
 
