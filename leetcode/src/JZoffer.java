a2422a88d0d91e46a352b5264aab9d8b2d74bac0
123 <897131608@qq.com>
2020-06-02 23:28:09 +0800

    栈的压入和弹出

--- leetcode/src/JZoffer.java
+++ leetcode/src/JZoffer.java
@@ -635,6 +635,21 @@ public class JZoffer {
         }
         return res;
     }
+    /*
+    栈的压入和弹出
+     */
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Stack<Integer> stack = new Stack<>();
+        int i = 0;
+        for(int num : pushed) {
+            stack.push(num); // num 入栈
+            while(!stack.isEmpty() && stack.peek() == popped[i]) { // 循环判断与出栈
+                stack.pop();
+                i++;
+            }
+        }
+        return stack.isEmpty();
+    }
 
 
 
