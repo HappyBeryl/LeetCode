5a4b83fdab1b79c55a1a510a616560c9fa7bbf56
123 <897131608@qq.com>
2020-06-14 23:04:35 +0800

    青蛙跳台阶动态规划

--- leetcode/src/TestDemo.java
+++ leetcode/src/TestDemo.java
@@ -2837,7 +2837,13 @@ public class TestDemo {
         return f2;
     }
 
-
+    public int JumpFloorII(int target) {
+        if(target <= 0) {
+            return 0;
+        }
+        //       return 1 << (target-1);
+        return (int) Math.pow(2, target-1);
+    }
 
 }
 
