668499022f33616ad36e6c603219fc5cdcaf056e
123 <897131608@qq.com>
2020-06-16 22:02:53 +0800

    提交测试习题代码

--- leetcode/src/TestDemo.java
+++ leetcode/src/TestDemo.java
@@ -2881,6 +2881,31 @@ public class TestDemo {
         return ans;
     }
 
+    public String getPermutation(int n, int k) {
+        List<String> res = new ArrayList<>();
+        StringBuilder solution = new StringBuilder();
+        boolean[] visited = new boolean[n];
+        backtrack(res,solution,n,k,visited);
+        return res.get(k-1);
+    }
+
+    void backtrack(List<String> res, StringBuilder solution,int n,int k,boolean[] visited){
+        if( res.size() == k) return;
+        if( solution.length() == n){
+            res.add(solution.toString());
+            return;
+        }
+        for(int i = 1; i <= n; i++){
+            if(visited[i-1]) continue;
+            visited[i-1] = true;
+            solution.append(i);
+            backtrack(res,solution,n,k,visited);
+            visited[i-1]=false;
+            solution.deleteCharAt(solution.length() -1);
+        }
+    }
+
+   
 }
 
 
