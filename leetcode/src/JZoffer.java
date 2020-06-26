6980d5044fa803b140f4d9b8f8e4bafa3f266194
123 <897131608@qq.com>
2020-06-26 22:48:00 +0800

    提交测试代码

--- leetcode/src/JZoffer.java
+++ leetcode/src/JZoffer.java
@@ -785,6 +785,18 @@ public class JZoffer {
         return null;
     }
 
+    public int maxSubArray(int[] nums) {
+        int maxSum = nums[0];
+        int curSum = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            curSum = Math.max(curSum + nums[i], nums[i]);
+        }
+        if (curSum > maxSum) {
+            maxSum = curSum;
+        }
+        return maxSum;
+    }
+
 
 
 
