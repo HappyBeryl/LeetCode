f552c96f75beb9466ec0912b1d08bbea095ed2ac
123 <897131608@qq.com>
2020-06-30 22:14:11 +0800

    提交测试习题代码

--- leetcode/src/TestDemo.java
+++ leetcode/src/TestDemo.java
@@ -3106,6 +3106,18 @@ public class TestDemo {
         return ans;
     }
 
+    public boolean canReach(int[] arr, int start) {
+        return dfs(arr, start);
+    }
+
+    private boolean dfs(int[] arr, int st) {
+        if (st < 0 || st >= arr.length || arr[st] == -1)
+            return false;
+        int step = arr[st];
+        arr[st] = -1;
+        return step == 0 || dfs(arr, st + step) || dfs(arr, st - step);
+    }
+
 
 
 
