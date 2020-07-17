dca5c471b6f12765bf2de3adc09e1c86ce50561d
123 <897131608@qq.com>
2020-07-17 20:42:21 +0800

    提交测试习题代码

--- leetcode/src/TestDemo.java
+++ leetcode/src/TestDemo.java
@@ -3237,9 +3237,19 @@ public class TestDemo {
                     temp++;
                 }
             }
+        }public int hammingWeight(int n) {
+            String str = Integer.toBinaryString(n);
+            int count=0;
+            for(int i=0;i<str.length();i++){
+                if(str.charAt(i)=='1')
+                    count++;
+            }
+            return count;
         }
         return maxLen;
     }
+
+
 
 
 
