eb1a717bf82a7b82ec2b95d18c451365afb70a44
123 <897131608@qq.com>
2020-08-09 17:34:08 +0800

    提交测试习题代码

--- leetcode/src/Main.java
+++ leetcode/src/Main.java
@@ -1,3 +1,7 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class Main {
 
@@ -12,6 +16,24 @@ public class Main {
         }
         System.out.println(sb);
     }
+    public static void func1(String str) {
+        String[] array = str.split(" ");
+        Map<String, Integer> map = new HashMap<>();
+        Set<String> set = new HashSet<>();
+        for (int i = 0; i < array.length; i++) {
+            if (set.add(array[i])) {
+                map.put(array[i], 1);
+            } else {
+                map.put(array[i], map.get(array[i])+1);
+            }
+        }
+        System.out.println(array.length); //单词总数
+        System.out.println(map.keySet()); //单词种类
+        for(Map.Entry<String,Integer> entry:map.entrySet()){ //出现次数
+            System.out.println(entry.getKey()+"->"+entry.getValue());
+        }
+    }
+
 
     public static void main(String[] args) {
         func("ABCBACAGFGFUNU");
