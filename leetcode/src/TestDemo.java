411623d5bb6f8b71bfe23f5f14c30acce92e134f
123 <897131608@qq.com>
2020-08-19 20:37:46 +0800

    提交修改

--- leetcode/src/TestDemo.java
+++ leetcode/src/TestDemo.java
@@ -3171,13 +3171,7 @@ public class TestDemo {
         process(root.right);
     }
 
-    /*
-      //第四种:直接通过Map.entrySet遍历key和value
-    for(Map.Entry<Integer,String> entry:map.entrySet()){
-        System.out.println(entry.getKey()+"->"+entry.getValue());
-    }
-    */
-
+   
 
 
 }
