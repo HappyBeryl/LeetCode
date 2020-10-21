cea0e38f5870752e1b47631e22afa9a0ee516eef
123 <897131608@qq.com>
2020-10-21 23:14:44 +0800

    和为0

--- leetcode/src/Main.java
+++ leetcode/src/Main.java
@@ -1,4 +1,32 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
+    public static void main(String[] args) {
+        int[] arr = new int[]{-1,0,1,2,-3};
+        List<List<Integer>> ret = find(arr);
+        System.out.println(ret);
+    }
+    public static List<List<Integer>> find(int[] arr) {
+        Arrays.sort(arr);
+        List<List<Integer>> ret = new ArrayList<>();
+        for (int i = 0; i < arr.length-2; i++) {
+            int j = i+1;
+            int k = arr.length-1;
+            while (j < k) {
+                if (arr[i]+arr[j]+arr[k]==0) {
+                    ret.add(Arrays.asList(arr[i], arr[j], arr[k]));
+                    j++;
+                    k--;
+                } else if (arr[i]+arr[j]+arr[k] > 0) {
+                    k--;
+                } else {
+                    j++;
+                }
+            }
+        }
+        return ret;
+    }
 
 }
